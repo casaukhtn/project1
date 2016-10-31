b17e92e8972ebc47bb566b86d239eb9d9ae7b536
phutruc95 <phutruc95@gmail.com>
2016-11-01 00:31:33 +0700

    update search result page

--- system/config.php
+++ system/config.php
@@ -1,11 +1,12 @@
-<?php 
-	$PAGES = array(); 
+<?php
+	$PAGES = array();
 	// các trang của user
 	$PAGES['default'] = "find.html";
 	$PAGES['find'] = "find.html";
 	$PAGES['login'] = "login.html";
 	$PAGES['register'] = "register.html";
 	$PAGES['404'] = "404.html";
+	$PAGES['findResult'] = "findResult.html"
 	// các trang admin
 	// cấu hình database
  ?>
